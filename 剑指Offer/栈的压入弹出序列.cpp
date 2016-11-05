a2c24eb25a214ad50a132023262ca811a3c8df35
MichoChan <15755396353@163.com>
2016-11-05 21:35:14 +0800

    stack exercise

---
+++ 剑指Offer/栈的压入弹出序列.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+         
+        if (pushV.size() == 0 || popV.size() == 0) return false;
+         
+        int idx = -1;
+        for (int i = 0; i < pushV.size(); ++i){
+            if (popV[0] == pushV[i]){
+                idx = i;
+            }
+        }
+         
+        if (idx < 0) return false;   //一开始这种情况没有考虑到
+         
+        int j = 1;
+         
+        vector<int> flag(pushV.size(),0);
+        flag[idx] = 1;
+         
+        while (j < popV.size()){
+            int tmpIdx = -1;
+            for (int i = 0; i < pushV.size(); ++i){
+                if (popV[j] == pushV[i]){
+                    tmpIdx = i;
+                }
+            }
+             
+            if (tmpIdx > idx){
+                idx = tmpIdx;
+            }else{
+                int i;
+                for (i = idx - 1; i >= 0; --i){
+                    if (flag[i] == 0){
+                        break;
+                    }
+                }
+                if (i < 0 || tmpIdx != i){
+                    return false;
+                }
+                idx = tmpIdx;
+            }
+             
+            flag[idx] = 1;
+            ++j;
+        }
+         
+        return true;
+    }
+};
+
+int main(){
+
+    Solution sol;
+
+    int a[] = {1,2,3,4,5};
+    int b[] = {4,3,5,1,2};
+
+    vector<int> pushV(a,a+5);
+    vector<int> popV(b,b+5);
+
+    cout << sol.IsPopOrder(pushV,popV)
+         << endl;
+
+    return 0;
+}
+
