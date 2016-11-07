ce9ec9e3066c52a080ab271fe4554f40e5c4b026
MichoChan <15755396353@163.com>
2016-11-07 10:03:33 +0800

    add a cpp

---
+++ 剑指Offer/二叉搜索树的后序遍历序列.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool VerifySquenceOfBST(vector<int> sequence) {
+        
+        int l = sequence.size();
+        if (l == 0){
+            return false;
+        }
+        
+        for (int i = 1; i < l; ++i){
+            if (sequence[i] > sequence[i-1]){
+                for (int j = i-1; j >= 0; --j){
+                    if (sequence[i] < sequence[j]){
+                        return false;
+                    }
+                }
+            }else{
+                int j;
+               for (j = i-1; j >= 0; --j){
+                    if (sequence[i] < sequence[j]){
+                    }else{
+                        break;
+                    }
+                }
+                for (;j >=0; --j){
+                    if (sequence[i] < sequence[j]){
+                        return false;
+                    }
+                }
+            }
+        }
+       
+        return true;
+    }
+};
